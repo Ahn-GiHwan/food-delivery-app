2252382be88a90ec35aee13397acaddba7feee8e
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-27 19:04:11 +0900

    feat: Create Orders component

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+function Orders() {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Orders</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Orders;
