d292074d85fe1a738d451a288164db9fc85d19b0
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-27 19:04:25 +0900

    feat: Create Settings component

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+function Settings() {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Settings;
