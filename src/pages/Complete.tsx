634dc7a53f55bf3effa17bbe57d949c1c0373105
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-27 19:03:17 +0900

    feat: Create Complete component

---
+++ src/pages/Complete.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+
+function Complete() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Complete</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Complete;
