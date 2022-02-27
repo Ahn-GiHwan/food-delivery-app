1e3903abd022b1159b1483c73f0856fd9c76bae5
Ahn-Gihwan <ahngh0113@naver.com>
2022-02-27 19:03:57 +0900

    feat: Create Ing component

---
+++ src/pages/Ing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+function Ing() {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Ing</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Ing;
