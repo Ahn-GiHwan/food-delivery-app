54bd5302d2b48e62141835abda33d5cab8f18320
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-01 01:09:19 +0900

    feat: Use orderSlice

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import {useSelector} from 'react-redux';
+import {RootState} from '../redux/store/reducer';
 
 function Orders() {
+  const {orders} = useSelector((state: RootState) => state.order);
   return (
     <SafeAreaView
       style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Orders</Text>
+      <Text>{orders.length}</Text>
     </SafeAreaView>
   );
 }
