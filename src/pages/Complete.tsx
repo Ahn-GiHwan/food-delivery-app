c09d7317615028e2c214b7ef9f6ab3fe3d255b98
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-02 22:42:53 +0900

    fix: Change style stylesheet to styled-components

--- src/pages/Complete.tsx
+++ src/pages/Complete.tsx
@@ -1,28 +1,55 @@
 import React, {useCallback, useState} from 'react';
-import {
-  Alert,
-  Dimensions,
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {SafeAreaView, Alert, Dimensions, Text} from 'react-native';
 import {
   NavigationProp,
   RouteProp,
   useNavigation,
   useRoute,
 } from '@react-navigation/native';
+import styled from 'styled-components/native';
 import {LoggedInParamList} from '../../App';
 import ImagePicker from 'react-native-image-crop-picker';
 import ImageResizer from 'react-native-image-resizer';
 import axios, {AxiosError} from 'axios';
-import Config from 'react-native-config';
 import {useSelector} from 'react-redux';
 import {RootState} from '../redux/store/reducer';
 import orderSlice from '../redux/slices/order';
 import {useAppDispatch} from '../redux/store';
+import useConfig from '../hooks/useConfig';
+
+const OrderId = styled.View`
+  padding: 20px;
+`;
+const Preview = styled.View`
+  width: ${Dimensions.get('window').width - 20}px;
+  height: ${Dimensions.get('window').height / 3}px;
+  margin-horizontal: 10px;
+  margin-bottom: 10px;
+  background-color: #d2d2d2;
+`;
+
+const PreviewImage = styled.Image.attrs({resizeMode: 'contain'})`
+  height: ${Dimensions.get('window').height / 3}px;
+`;
+
+const ButtonWrapper = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Button = styled.Pressable`
+  align-items: center;
+  width: 120px;
+  margin: 5px;
+  border-radius: 5px;
+  padding-horizontal: 20px;
+  padding-vertical: 10px;
+  background-color: ${({disabled}) => (disabled ? 'gray' : 'yellow')};
+`;
+
+const ButtonText = styled.Text`
+  color: black;
+`;
 
 function Complete() {
   const dispatch = useAppDispatch();
@@ -32,6 +59,7 @@ function Complete() {
     useState<{uri: string; name: string; type: string}>();
   const [preview, setPreview] = useState<{uri: string}>();
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const URL = useConfig();
 
   const onResponse = useCallback(async response => {
     console.log(response.width, response.height, response.exif);
@@ -58,9 +86,10 @@ function Complete() {
 
   const onTakePhoto = useCallback(() => {
     return ImagePicker.openCamera({
-      includeBase64: true,
-      includeExif: true,
-      saveToPhotos: true,
+      includeBase64: true, // 미리보기 보시를 위해
+      includeExif: true, // 찍은 핸드폰 방향에 맞춰서 사진이 자동으로 회전해줌
+      cropping: true,
+      // saveToPhotos: true,
     })
       .then(onResponse)
       .catch(console.log);
@@ -90,7 +119,10 @@ function Complete() {
     formData.append('image', image);
     formData.append('orderId', orderId);
     try {
-      await axios.post(`${Config.API_URL}/complete`, formData, {
+      await axios({
+        url: `${URL}/complete`,
+        method: 'POST',
+        data: formData,
         headers: {
           authorization: `Bearer ${accessToken}`,
         },
@@ -105,68 +137,27 @@ function Complete() {
         Alert.alert('알림', errorResponse.data.message);
       }
     }
-  }, [dispatch, navigation, image, orderId, accessToken]);
+  }, [image, orderId, URL, accessToken, navigation, dispatch]);
 
   return (
-    <View>
-      <View style={styles.orderId}>
+    <SafeAreaView>
+      <OrderId>
         <Text>주문번호: {orderId}</Text>
-      </View>
-      <View style={styles.preview}>
-        {preview && <Image style={styles.previewImage} source={preview} />}
-      </View>
-      <View style={styles.buttonWrapper}>
-        <Pressable style={styles.button} onPress={onTakePhoto}>
-          <Text style={styles.buttonText}>이미지 촬영</Text>
-        </Pressable>
-        <Pressable style={styles.button} onPress={onChangeFile}>
-          <Text style={styles.buttonText}>이미지 선택</Text>
-        </Pressable>
-        <Pressable
-          style={
-            image
-              ? styles.button
-              : StyleSheet.compose(styles.button, styles.buttonDisabled)
-          }
-          onPress={onComplete}>
-          <Text style={styles.buttonText}>완료</Text>
-        </Pressable>
-      </View>
-    </View>
+      </OrderId>
+      <Preview>{preview && <PreviewImage source={preview} />}</Preview>
+      <ButtonWrapper>
+        <Button onPress={onTakePhoto}>
+          <ButtonText>이미지 촬영</ButtonText>
+        </Button>
+        <Button onPress={onChangeFile}>
+          <ButtonText>이미지 선택</ButtonText>
+        </Button>
+        <Button onPress={onComplete} disabled={image ? false : true}>
+          <ButtonText>완료</ButtonText>
+        </Button>
+      </ButtonWrapper>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  orderId: {
-    padding: 20,
-  },
-  preview: {
-    marginHorizontal: 10,
-    width: Dimensions.get('window').width - 20,
-    height: Dimensions.get('window').height / 3,
-    backgroundColor: '#D2D2D2',
-    marginBottom: 10,
-  },
-  previewImage: {
-    height: Dimensions.get('window').height / 3,
-    resizeMode: 'contain',
-  },
-  buttonWrapper: {flexDirection: 'row', justifyContent: 'center'},
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    width: 120,
-    alignItems: 'center',
-    backgroundColor: 'yellow',
-    borderRadius: 5,
-    margin: 5,
-  },
-  buttonText: {
-    color: 'black',
-  },
-  buttonDisabled: {
-    backgroundColor: 'gray',
-  },
-});
 
 export default Complete;
